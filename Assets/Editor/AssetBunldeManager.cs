afbd31a968280ee2eff48136f96de593c23b79af
RuichangBao <1294838346@qq.com>
2020-03-23 16:36:03 +0800

    第四次修改

--- Assets/Editor/AssetBunldeManager.cs
+++ Assets/Editor/AssetBunldeManager.cs
@@ -93,7 +93,7 @@ public class AssetBunldeManager//:Editor
         AssetBundleBuild abBuild = new AssetBundleBuild();
         abBuild.assetBundleName = "123456"; // 就是我们要的目标AssetLabel
         abBuild.assetBundleName = "78945"; // 就是我们要的目标AssetLabel
-        Debug.Log("第二次修改");
+        Debug.Log("第四次修改");
         //dfsadfa
     }
 }
